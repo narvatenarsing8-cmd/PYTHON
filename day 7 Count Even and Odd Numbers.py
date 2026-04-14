3619fbd25dc0988b572ea7a1d139b9b9607600d2
Narsing Narvate <narvatenarsing8@gmail.com>
2026-04-14 23:24:51 +0530

    Add files via upload

---
+++ day 7 Count Even and Odd Numbers.py
@@ -0,0 +1,12 @@
+arr = list(map(int, input("Enter numbers: ").split()))
+
+even = odd = 0
+
+for i in arr:
+    if i % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+
+print("Even:", even)
+print("Odd:", odd)
