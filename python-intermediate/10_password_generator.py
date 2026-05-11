793617e91e4b635109b51258ab960b8a59bc9fd5
Narsing Narvate <narvatenarsing8@gmail.com>
2026-05-11 22:43:35 +0530

    Create 10_password_generator.py
    Add password generator

---
+++ python-intermediate/10_password_generator.py
@@ -0,0 +1,9 @@
+import random
+import string
+
+length = 6
+chars = string.ascii_letters + string.digits
+
+password = ''.join(random.choice(chars) for _ in range(length))
+
+print("Password:", password)
