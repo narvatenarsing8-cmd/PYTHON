6be597865d1f531225f9d9ae4d14b21ed174547f
Narsing Narvate <narvatenarsing8@gmail.com>
2026-05-24 21:54:21 +0530

    Create 10_match_case.py
    Add match case example

---
+++ control-statements/10_match_case.py
@@ -0,0 +1,14 @@
+day = int(input("Enter day number: "))
+
+match day:
+    case 1:
+        print("Monday")
+
+    case 2:
+        print("Tuesday")
+
+    case 3:
+        print("Wednesday")
+
+    case _:
+        print("Invalid")
