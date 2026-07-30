8872a80e90bd936859d130fcf3017ea3d519aec7
Narsing Narvate <narvatenarsing8@gmail.com>
2026-07-30 21:36:28 +0530

    Create palindrome.py
    Add Python program to check palindrome number

---
+++ palindrome.py
@@ -0,0 +1,16 @@
+# Program to check whether a number is a palindrome
+
+num = int(input("Enter a number: "))
+
+temp = num
+rev = 0
+
+while temp > 0:
+    digit = temp % 10
+    rev = rev * 10 + digit
+    temp = temp // 10
+
+if num == rev:
+    print("Palindrome Number")
+else:
+    print("Not a Palindrome Number")
