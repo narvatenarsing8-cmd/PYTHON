6bd7489450553a9ee2f7db83f8d644bc996ee9bd
Narsing Narvate <narvatenarsing8@gmail.com>
2026-08-03 18:58:03 +0530

    Create armstrong_number.py
    Add Python program to check Armstrong number

---
+++ armstrong_number.py
@@ -0,0 +1,16 @@
+# Program to check whether a number is an Armstrong number
+
+num = int(input("Enter a number: "))
+
+temp = num
+sum = 0
+
+while temp > 0:
+    digit = temp % 10
+    sum = sum + (digit ** 3)
+    temp = temp // 10
+
+if num == sum:
+    print("Armstrong Number")
+else:
+    print("Not an Armstrong Number")
